5b4a61d8be8fb0a5cdca94fbbe50ecbe7f8e4a2b
agnesmonica07 <agnesmonica071205@gmail.com>
2025-10-04 17:43:14 +0700

    make api

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import express from "express"
 import web from "./routes/web.js"
+import api from "./routes/api.js"  
 
 const app = express()
 
-app.use (express.json())
-
+app.use(express.json())
 app.use(express.static('public'))
-
 app.set('view engine', 'ejs')
 
+// routing
 app.use(web)
+app.use('/api', api)
 
 app.listen(3000, () => {
     console.log(`Aplikasi berjalan di http://localhost:3000`)
