76ff678bd977005ba7c3bda76b655f3ad783b19e
agnesmonica07 <agnesmonica071205@gmail.com>
2025-10-04 18:42:07 +0700

    ct projek to postman&mongosh

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,18 @@
+import UserModel from "../models/userModel";
+
 export const register = async(requestAnimationFrame,res)=>{
     try{
+        //utk mengambil body atau data dari request
+        const registerData = requestAnimationFrame.body
+
+        console.log(registerData);
+
+        await UserModel.create({
+            username : registerData.username,
+            email : resisterData.email,
+            password : registerData.password
+        })
+
         res.status(201).json({
             message : "Berhasil register, silakan login",
             date : null
